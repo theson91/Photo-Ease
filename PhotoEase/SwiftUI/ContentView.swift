a59c2bf15e6910bc94303404f7a61578a944b076
Son <sonbt178@gmail.com>
2025-05-08 21:09:29 +0700

    Update Xcode Cloud integration

--- PhotoEase/SwiftUI/ContentView.swift
+++ PhotoEase/SwiftUI/ContentView.swift
@@ -5,7 +5,10 @@ struct ContentView: UIViewControllerRepresentable {
     typealias UIViewControllerType = UINavigationController
 
     func makeUIViewController(context: Context) -> UINavigationController {
-        let viewController = PhotosListViewController(nibName: "PhotosListViewController", bundle: nil)
+//        let viewController = PhotosListViewController(nibName: "PhotosListViewController", bundle: nil)
+        let viewController = UIViewController()
+        viewController.view.frame = UIScreen.main.bounds
+        viewController.view.backgroundColor = UIColor.yellow
         let navigationController = UINavigationController(rootViewController: viewController)
         return navigationController
     }
